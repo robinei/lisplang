daba7d224bc003a472f4d57e57403583eea3a760
Robin Eidissen <robinei@gmail.com>
2018-04-01 15:39:45 +0200

    support direct threaded interpreter (on GCC)

--- main.c
+++ main.c
@@ -543,7 +543,7 @@ enum {
     FOR_ALL_NUM(DEF_SUB_ENUM)
     FOR_ALL_NUM(DEF_MUL_ENUM)
     FOR_ALL_NUM(DEF_DIV_ENUM)
-    FOR_ALL_NUM(DEF_MOD_ENUM)
+    FOR_ALL_INT(DEF_MOD_ENUM)
     FOR_ALL_PRIM(DEF_EQ_ENUM)
     FOR_ALL_NUM(DEF_LT_ENUM)
     FOR_ALL_NUM(DEF_GT_ENUM)
@@ -578,81 +578,137 @@ enum {
 #define MK_INSTR_BC(op, bc)         ((op) | ((uint64_t)(bc) << (INSTR_OP_BITS + INSTR_A_BITS)))
 
 
+#ifdef __GNUC__
+#define DEF_PRINT_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_PRINT_ ## UNAME,
+#define DEF_LIT_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_LIT_ ## UNAME,
+#define DEF_ADD_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_ADD_ ## UNAME,
+#define DEF_SUB_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_SUB_ ## UNAME,
+#define DEF_MUL_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_MUL_ ## UNAME,
+#define DEF_DIV_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_DIV_ ## UNAME,
+#define DEF_MOD_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_MOD_ ## UNAME,
+#define DEF_EQ_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_EQ_ ## UNAME,
+#define DEF_LT_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_LT_ ## UNAME,
+#define DEF_GT_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_GT_ ## UNAME,
+#define DEF_LTEQ_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_LTEQ_ ## UNAME,
+#define DEF_GTEQ_LABEL(UNAME, LNAME, TYPE, FMT) &&label_ ## OP_GTEQ_ ## UNAME,
+
+#define DISPATCH_CASE(OPNAME) label_ ## OPNAME:
+#define DISPATCH_NEXT() instr = *ip++; goto *dispatch_table[INSTR_OP(instr)];
+#define BEGIN_DISPATCH() \
+    static void *dispatch_table[] = { \
+        NULL, \
+        NULL, \
+        NULL, \
+        NULL, \
+        &&label_OP_JUMP, \
+        &&label_OP_JFALSE, \
+        &&label_OP_JTRUE, \
+        &&label_OP_TCALL, \
+        &&label_OP_CALL, \
+        &&label_OP_RET, \
+        &&label_OP_MOVE, \
+        FOR_ALL_PRIM(DEF_PRINT_LABEL) \
+        FOR_ALL_PRIM(DEF_LIT_LABEL) \
+        &&label_ ## OP_NOT_BOOL, \
+        FOR_ALL_NUM(DEF_ADD_LABEL) \
+        FOR_ALL_NUM(DEF_SUB_LABEL) \
+        FOR_ALL_NUM(DEF_MUL_LABEL) \
+        FOR_ALL_NUM(DEF_DIV_LABEL) \
+        FOR_ALL_INT(DEF_MOD_LABEL) \
+        FOR_ALL_PRIM(DEF_EQ_LABEL) \
+        FOR_ALL_NUM(DEF_LT_LABEL) \
+        FOR_ALL_NUM(DEF_GT_LABEL) \
+        FOR_ALL_NUM(DEF_LTEQ_LABEL) \
+        FOR_ALL_NUM(DEF_GTEQ_LABEL) \
+    }; \
+    uint64_t instr; \
+    DISPATCH_NEXT()
+#define END_DISPATCH()
+
+#else
+
+#define DISPATCH_CASE(OPNAME) case OPNAME:
+#define DISPATCH_NEXT() continue;
+#define BEGIN_DISPATCH() \
+    for (;;) { \
+        uint64_t instr = *ip++; \
+        switch (INSTR_OP(instr)) {
+#define END_DISPATCH() \
+        } \
+    }
+
+#endif
+
 #define DEFINE_PRINT_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_PRINT_ ## UNAME:  printf("printed: " FMT "\n", *(TYPE  *)(fp + INSTR_A(instr))); continue;
+    DISPATCH_CASE(OP_PRINT_ ## UNAME)  printf("printed: " FMT "\n", *(TYPE  *)(fp + INSTR_A(instr))); DISPATCH_NEXT();
 #define DEFINE_LIT_32_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_LIT_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = (TYPE)INSTR_BC(instr); continue;
+    DISPATCH_CASE(OP_LIT_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = (TYPE)INSTR_BC(instr); DISPATCH_NEXT();
 #define DEFINE_LIT_64_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_LIT_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = (TYPE)*ip++; continue;
+    DISPATCH_CASE(OP_LIT_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = (TYPE)*ip++; DISPATCH_NEXT();
 #define DEFINE_ADD_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_ADD_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) +  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_ADD_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) +  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_SUB_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_SUB_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) -  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_SUB_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) -  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_MUL_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_MUL_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) *  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_MUL_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) *  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_DIV_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_DIV_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) /  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_DIV_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) /  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_MOD_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_MOD_ ## UNAME:   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) %  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_MOD_ ## UNAME)   *(TYPE  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) %  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_EQ_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_EQ_ ## UNAME:    *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) == *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_EQ_ ## UNAME)    *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) == *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_LT_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_LT_ ## UNAME:    *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) <  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_LT_ ## UNAME)    *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) <  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_GT_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_GT_ ## UNAME:    *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) >  *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_GT_ ## UNAME)    *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) >  *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_LTEQ_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_LTEQ_ ## UNAME:  *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) <= *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_LTEQ_ ## UNAME)  *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) <= *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 #define DEFINE_GTEQ_INSTR(UNAME, LNAME, TYPE, FMT) \
-    case OP_GTEQ_ ## UNAME:  *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) >= *(TYPE  *)(fp + INSTR_C(instr)); continue;
+    DISPATCH_CASE(OP_GTEQ_ ## UNAME)  *(bool  *)(fp + INSTR_A(instr)) = *(TYPE  *)(fp + INSTR_B(instr)) >= *(TYPE  *)(fp + INSTR_C(instr)); DISPATCH_NEXT();
 
 static uint64_t *print_instr(uint64_t *code);
 
+
 void interpret(uint64_t *ip, uint64_t *fp) {
-    for(;;) {
-        //printf("exec: "); print_instr(ip);
-        uint64_t instr = *ip++;
+    BEGIN_DISPATCH()
+    DISPATCH_CASE(OP_JUMP) ip += (int32_t)INSTR_BC(instr) - 1; DISPATCH_NEXT();
+    DISPATCH_CASE(OP_JFALSE) if (!*(bool *)(fp + INSTR_A(instr))) { ip += (int32_t)INSTR_BC(instr) - 1; } DISPATCH_NEXT();
+    DISPATCH_CASE(OP_JTRUE)  if ( *(bool *)(fp + INSTR_A(instr))) { ip += (int32_t)INSTR_BC(instr) - 1; } DISPATCH_NEXT();
 
-        switch (INSTR_OP(instr)) {
-        case OP_JUMP: ip += (int32_t)INSTR_BC(instr) - 1; continue;
-        case OP_JFALSE: if (!*(bool *)(fp + INSTR_A(instr))) { ip += (int32_t)INSTR_BC(instr) - 1; } continue;
-        case OP_JTRUE:  if ( *(bool *)(fp + INSTR_A(instr))) { ip += (int32_t)INSTR_BC(instr) - 1; } continue;
-
-        case OP_TCALL: {
-            uint32_t fun_offset = INSTR_A(instr);
-            Function *fun = *(Function **)(fp + fun_offset);
-            fp += fun_offset;
-            ip = fun->code; /* tail call means we just overwrite these, and don't grow the call stack */
-            continue;
-        }
-        case OP_CALL: {
-            uint32_t fun_offset = INSTR_A(instr);
-            uint32_t result_reg = INSTR_B(instr);
-            Function *fun = *(Function **)(fp + fun_offset);
-            interpret(fun->code, fp + fun_offset); /* just use the C call stack */
-            *(uint64_t *)(fp + result_reg) = *(uint64_t *)(fp + fun_offset);
-            continue;
-        }
-        case OP_RET: return; /* since we use the C call stack we just return */
-
-        case OP_MOVE: *(uint64_t *)(fp + INSTR_A(instr)) = *(uint64_t *)(fp + INSTR_B(instr)); continue;
-
-        FOR_ALL_PRIM(DEFINE_PRINT_INSTR)
-        FOR_ALL_PRIM_32(DEFINE_LIT_32_INSTR)
-        FOR_ALL_PRIM_64(DEFINE_LIT_64_INSTR)
-        case OP_NOT_BOOL: *(bool *)(fp + INSTR_A(instr)) = !*(bool *)(fp + INSTR_B(instr)); continue;
-        FOR_ALL_NUM(DEFINE_ADD_INSTR)
-        FOR_ALL_NUM(DEFINE_SUB_INSTR)
-        FOR_ALL_NUM(DEFINE_MUL_INSTR)
-        FOR_ALL_NUM(DEFINE_DIV_INSTR)
-        FOR_ALL_INT(DEFINE_MOD_INSTR)
-        FOR_ALL_PRIM(DEFINE_EQ_INSTR)
-        FOR_ALL_NUM(DEFINE_LT_INSTR)
-        FOR_ALL_NUM(DEFINE_GT_INSTR)
-        FOR_ALL_NUM(DEFINE_LTEQ_INSTR)
-        FOR_ALL_NUM(DEFINE_GTEQ_INSTR)
-        }
+    DISPATCH_CASE(OP_TCALL) {
+        uint32_t fun_offset = INSTR_A(instr);
+        Function *fun = *(Function **)(fp + fun_offset);
+        fp += fun_offset;
+        ip = fun->code; /* tail call means we just overwrite these, and don't grow the call stack */
+        DISPATCH_NEXT();
     }
+    DISPATCH_CASE(OP_CALL) {
+        uint32_t fun_offset = INSTR_A(instr);
+        uint32_t result_reg = INSTR_B(instr);
+        Function *fun = *(Function **)(fp + fun_offset);
+        interpret(fun->code, fp + fun_offset); /* just use the C call stack */
+        *(uint64_t *)(fp + result_reg) = *(uint64_t *)(fp + fun_offset);
+        DISPATCH_NEXT();
+    }
+    DISPATCH_CASE(OP_RET) return; /* since we use the C call stack we just return */
 
+    DISPATCH_CASE(OP_MOVE) *(uint64_t *)(fp + INSTR_A(instr)) = *(uint64_t *)(fp + INSTR_B(instr)); DISPATCH_NEXT();
+
+    FOR_ALL_PRIM(DEFINE_PRINT_INSTR)
+    FOR_ALL_PRIM_32(DEFINE_LIT_32_INSTR)
+    FOR_ALL_PRIM_64(DEFINE_LIT_64_INSTR)
+    DISPATCH_CASE(OP_NOT_BOOL) *(bool *)(fp + INSTR_A(instr)) = !*(bool *)(fp + INSTR_B(instr)); DISPATCH_NEXT();
+    FOR_ALL_NUM(DEFINE_ADD_INSTR)
+    FOR_ALL_NUM(DEFINE_SUB_INSTR)
+    FOR_ALL_NUM(DEFINE_MUL_INSTR)
+    FOR_ALL_NUM(DEFINE_DIV_INSTR)
+    FOR_ALL_INT(DEFINE_MOD_INSTR)
+    FOR_ALL_PRIM(DEFINE_EQ_INSTR)
+    FOR_ALL_NUM(DEFINE_LT_INSTR)
+    FOR_ALL_NUM(DEFINE_GT_INSTR)
+    FOR_ALL_NUM(DEFINE_LTEQ_INSTR)
+    FOR_ALL_NUM(DEFINE_GTEQ_INSTR)
+    END_DISPATCH();
 }
 
 #define INAME_FMT "%-12s"
@@ -687,19 +743,19 @@ static uint64_t *print_instr(uint64_t *code) {
     case OP_RET: printf("ret\n"); break;
     case OP_MOVE: printf(INAME_FMT "r%u <- r%u\n", "move", INSTR_A(instr), INSTR_B(instr)); break;
     case OP_NOT_BOOL: printf(INAME_FMT "r%u <- r%u\n", "not/bool", INSTR_A(instr), INSTR_B(instr)); break;
-        FOR_ALL_PRIM(DEFINE_PRINT_PRINT)
-            FOR_ALL_PRIM_32(PRINT_PRINT_LIT_32)
-            FOR_ALL_PRIM_64(PRINT_PRINT_LIT_64)
-            FOR_ALL_NUM(DEFINE_PRINT_ADD)
-            FOR_ALL_NUM(DEFINE_PRINT_SUB)
-            FOR_ALL_NUM(DEFINE_PRINT_MUL)
-            FOR_ALL_NUM(DEFINE_PRINT_DIV)
-            FOR_ALL_INT(DEFINE_PRINT_MOD)
-            FOR_ALL_PRIM(DEFINE_PRINT_EQ)
-            FOR_ALL_NUM(DEFINE_PRINT_LT)
-            FOR_ALL_NUM(DEFINE_PRINT_GT)
-            FOR_ALL_NUM(DEFINE_PRINT_LTEQ)
-            FOR_ALL_NUM(DEFINE_PRINT_GTEQ)
+    FOR_ALL_PRIM(DEFINE_PRINT_PRINT)
+    FOR_ALL_PRIM_32(PRINT_PRINT_LIT_32)
+    FOR_ALL_PRIM_64(PRINT_PRINT_LIT_64)
+    FOR_ALL_NUM(DEFINE_PRINT_ADD)
+    FOR_ALL_NUM(DEFINE_PRINT_SUB)
+    FOR_ALL_NUM(DEFINE_PRINT_MUL)
+    FOR_ALL_NUM(DEFINE_PRINT_DIV)
+    FOR_ALL_INT(DEFINE_PRINT_MOD)
+    FOR_ALL_PRIM(DEFINE_PRINT_EQ)
+    FOR_ALL_NUM(DEFINE_PRINT_LT)
+    FOR_ALL_NUM(DEFINE_PRINT_GT)
+    FOR_ALL_NUM(DEFINE_PRINT_LTEQ)
+    FOR_ALL_NUM(DEFINE_PRINT_GTEQ)
     }
     return code + 1;
 }
@@ -1662,7 +1718,7 @@ end:
 int main(int argc, char *argv[]) {
     init_globals();
 
-    uint32_t iters = 10000000;
+    uint32_t iters = 100000000;
     if (argc > 1) {
         iters = atoi(argv[1]);
     }
